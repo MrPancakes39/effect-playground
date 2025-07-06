07a978ef98879caa0419ba721fc99560322d1dee
MrPancakes39 <salmannhassan39@gmail.com>
2025-07-06 23:00:02 +0300

    Init Simple Hono Server

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { serve } from "srvx";
+
+import { Hono } from "hono";
+
+const app = new Hono();
+app.get("/", (c) => c.text("Hello via Bun!\n"));
+
+serve({
+  fetch: app.fetch,
+  port: 3000,
+});
